2a107c2369d7c68c78855c5d96a9a49e5ab2a2b9
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-05 02:36:47 -0400

    minmaxsum made

---
+++ interview_prep_kit/src/minMaxSum.js
@@ -0,0 +1,14 @@
+
+n miniMaxSum(arr) {
+	    // Write your code here
+	//     // find the min value of the array and the max value of the array and then exclude the minimum
+	//         let minimum = arr[0]; let maximum = 0; let sum = 0;
+	//             arr.forEach(element => {
+	//                     if (element < minimum){minimum = element};
+	//                             if (element > maximum){maximum = element};
+	//                                     sum += element;
+	//                                         })
+	//                                             const maxSum = sum - minimum; 
+	//                                                 const minSum = sum - maximum;
+	//                                                     console.log(minSum, maxSum)
+	//                                                     }
